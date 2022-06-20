c71b0e4534a83d448f3a6c64c62f85d24eb77263
dongsun1 <le12352@naver.com>
2022-06-20 11:03:26 +0900

    cors 추가

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 8001;
 
 const chartRouter = require("./routes/chart");
 
 app.use("/api", [chartRouter]);
+
+app.use(cors());
 
 app.listen(port, () => {
   console.log(port, "포트로 서버가 켜졌어요!");
